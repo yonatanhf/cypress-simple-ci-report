2defb311f1e7b364f1794be2588f95a0698f3fe1
Yonatan Fessehaye <fessehayeyonatan@gmail.com>
2020-11-16 11:58:08 +0100

    removed old tests & video generators

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -23,10 +23,10 @@ import addContext from 'mochawesome/addContext'
 
 Cypress.on("test:after:run", (test, runnable) => {
     
-    let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
+    // let videoName = Cypress.spec.name
+    // videoName = videoName.replace('/.js.*', '.js')
+    // const videoUrl = 'videos/' + videoName + '.mp4'
 
-    addContext({ test }, videoUrl)
+    // addContext({ test }, videoUrl)
 });
 
